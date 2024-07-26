446159166fbfcd397de3743b8b2d3cbae53e03ae
seanbsu <seancalkins@u.boisestate.edu>
2024-07-26 09:48:22 -0600

    act 7

--- JS/javascript-exercises/07_tempConversion/tempConversion.js
+++ JS/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  cTemp = (fTemp-32) * (5/9);
+  return Number(cTemp.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  fTemp = cTemp * (9/5) +32;
+  return Number(fTemp.toFixed(1))
 };
 
 // Do not edit below this line
