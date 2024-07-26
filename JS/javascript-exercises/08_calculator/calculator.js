2702a5ae1ee71112197505c1ce6e044de7a99fa8
seanbsu <seancalkins@u.boisestate.edu>
2024-07-26 11:32:50 -0600

    8 and 9 complete

--- JS/javascript-exercises/08_calculator/calculator.js
+++ JS/javascript-exercises/08_calculator/calculator.js
@@ -1,24 +1,49 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length === 0){
+    return 0;
+  }
+	 let sum = 0;
+  for(num of arr){
+    sum+=num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length === 0){
+    return 0;
+  }
+  let sol = 1;
+  for(num of arr){
+    sol*=num;
+  }
+  return sol;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if(num < 0){
+    return 'ERROR'
+  }
+  if( num === 0){
+    return 1;
+  }
+  let sol = 1;
+  for(let i =1; i <= num; i++){
+    sol*=i;
+  }
+  return sol
 	
 };
 
--- JS/javascript-exercises/09_palindromes/palindromes.js
+++ JS/javascript-exercises/09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+// const palindromes = function (string) {
+//     stringArray = [];
+//     for(let i=0; i<string.length; i++){
+//         let charCode = string.charCodeAt(i);
+//         if ((charCode > 47 && charCode < 58) ||  (charCode > 96 && charCode < 123) ||    (charCode > 64 && charCode < 91)
+//         ){
+//             stringArray.push(string[i]);
+//          }   
+//     }
+//     formattedString = stringArray.join('').toLowerCase();
+//     reversedString = stringArray.reverse().join('').toLowerCase();
+//     return formattedString == reversedString; 
+// };
+const palindromes = function (string) {
+    // Since we only consider letters and numbers, create a variable containing all valid characters
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  
+    // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
+    const cleanedString = string
+      .toLowerCase()
+      .split('')
+      .filter((character) => alphanumerical.includes(character))
+      .join('');
+  
+    // Create a new reversed string for comparison
+    const reversedString = cleanedString.split('').reverse().join('');
+  
+    // Return the outcome of the comparison which will either be true or false
+    return cleanedString === reversedString;
+  };
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
