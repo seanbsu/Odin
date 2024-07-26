a100d98e6e6fb03da8e9c53d2381de8dfeb3ec9b
seanbsu <seancalkins@u.boisestate.edu>
2024-07-26 11:52:14 -0600

    act 10 complete

--- JS/javascript-exercises/10_fibonacci/fibonacci.js
+++ JS/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num)
+    if(num === 0){
+        return 0;
+    }
+    if(num < 0){
+        return 'OOPS'
+    }
+    let i = 1, secondPrev = 0, prev =1, fibNum = 1;
+    while(i<num){
+        fibNum = secondPrev + prev;
+        secondPrev = prev;
+        prev = fibNum
+        i++;
+    }
+    return fibNum;
 };
 
 // Do not edit below this line
