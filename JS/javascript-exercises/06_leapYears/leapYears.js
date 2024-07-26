7e34f1216a1f79bb4e5c271042d885a71998021d
seanbsu <seancalkins@u.boisestate.edu>
2024-07-26 09:18:03 -0600

    activity 06 complete

--- JS/javascript-exercises/06_leapYears/leapYears.js
+++ JS/javascript-exercises/06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    isCentury = year % 100 === 0
+    isYearDivisibleByFour = year % 4 === 0 
+    isYearDivisibleByFourHundred = year % 400 === 0
+    if(isYearDivisibleByFour &&
+      (!isCentury || isYearDivisibleByFourHundred)){
+            return true
+        }
+    return false;
 
 };
 
